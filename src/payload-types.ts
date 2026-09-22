1ed0c6252bee34e40cdc01c04fb845d220ad7711
Kasper Brok <kasperbrok@gmail.com>
2026-09-22 11:09:54 +0200

    update hero name

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -184,7 +184,7 @@ export interface Page {
   id: number;
   title: string;
   hero: {
-    type: 'none' | 'highImpact' | 'mediumImpact' | 'lowImpact';
+    type: 'none' | 'logoHero' | 'mediumImpact' | 'lowImpact';
     richText?: {
       root: {
         type: string;
